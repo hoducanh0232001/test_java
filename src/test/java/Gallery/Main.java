d74d34fa8b1c8f522a20dd97db10876758da3e1a
nam09082001 <106221921+nam09082001@users.noreply.github.com>
2022-07-17 20:33:06 +0700

    modified

--- src/test/java/Gallery/Main.java
+++ src/test/java/Gallery/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Alo");
+        System.out.println("sss");
        Media media = new Media(1, "Hello", "Ducanh", "4MB");
         // Media media2 = new Media(2,"Khong co gi","Ducanh","6MB");
 
